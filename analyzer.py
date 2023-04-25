0ed1de3458f640741743107b06ef4b46833c1a4d
Priscigs <fatimag811@gmail.com>
2023-04-25 13:24:54 -0600

    🫱🏽‍🫲🏼 Some other changes in Yalex files

--- analyzer.py
+++ analyzer.py
@@ -14,6 +14,7 @@ print(expresion_total)
 
 archivo_input = "2 \n2342 +  2 * 3 = 2350\n323.123 - -21.35 = 301.773\n0x3F - 0x1A = 0x25\n4 ^ 2 / 4 = 4"
 # archivo_input = "10 20.5 0xA1B2 + - * / ^\n42 3.14 0x12345 * / ^ + -"
+# archivo_input = "9 8.5 0xA1B2 + - * / ^ 2 1\n4 3.1 0x12345 * / ^ + - 5 ?"
 # archivo_input = "a1 b2 for if 10 20\nwhile sum123 + - * /"
 
 def analizar(input):
--- compilado.py
+++ compilado.py
@@ -1,20 +1,32 @@
 import re
 
-ws = r"([' ''\t''\n'])+"
-id = r"(['A'-'Z''a'-'z'])((['A'-'Z''a'-'z'])|(['0'-'9']))*"
+entero = r"0|1|2|3|4|5|6|7|8|9"
+decimal = r"(0|1|2|3|4|5|6|7|8|9).(0|1|2|3|4|5|6|7|8|9)"
+hexadecimal = r"0[xX][0-9a-fA-F]+"
+operador = r"\+|\-|\*"
+potenciacion = r"\^"
+tabulaciones = r"\t|\n "
 
 # Compilación de todas las expresiones regulares en una sola expresión
-expresion_total = re.compile(f"({ws}|{id})")
+expresion_total = re.compile(f"({entero}|{decimal}|{hexadecimal}|{operador}|{potenciacion}|{tabulaciones})")
 
 # Analizar el archivo de entrada
-archivo_entrada = "22342 +  2 * 3 = 2350323.123 - -21.35 = 301.773 0x3F - 0x1A = 0x254 ^ 2 / 4 = 4"
+archivo_entrada = "a1 b2 for if 10 20while sum123 + - * /"
 def analizar(entrada):
     tokens = entrada.split()
     for token in tokens:
-        if re.match(ws, token):
-            print(f"Ws: {token}")
-        elif re.match(id, token):
-            print(f"Id: {token}")
+        if re.match(entero, token):
+            print(f"Entero: {token}")
+        elif re.match(decimal, token):
+            print(f"Decimal: {token}")
+        elif re.match(hexadecimal, token):
+            print(f"Hexadecimal: {token}")
+        elif re.match(operador, token):
+            print(f"Operador: {token}")
+        elif re.match(potenciacion, token):
+            print(f"Potenciacion: {token}")
+        elif re.match(tabulaciones, token):
+            print(f"Tabulaciones: {token}")
         else:
             print(f"Error Sintactico(No reconocido): {token}")
 
